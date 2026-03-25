e47c6a676434d8feeffc3e9c50f78c2674ba6a80
dwrankit545 <dwr.ankit545@gmail.com>
2026-03-25 13:00:56 +0530

    removed background color on body element

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,11 +27,7 @@ export default async function RootLayout({ children }: PropsWithChildren) {
           crossOrigin={'anonymous'}
         />
       </head>
-      <body className={clsx(primary.variable, 'font-primary')} style={{
-    backgroundColor: "red",
-    padding: "10px",
-    borderRadius: "8px",
-  }}>
+      <body className={clsx(primary.variable, 'font-primary')}>
         {draftMode().isEnabled ? (
           <PreviewProvider token={token}>
             <Layout>{children}</Layout>
